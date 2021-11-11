56b8610ea1d264bb81730aa2db5f00ea4d7e4312
catherinebenge <cgb355@nyu.edu>
2021-11-10 22:24:08 -0500

    fixed doors and hovers and fixed mg

--- script.js
+++ script.js
@@ -97,7 +97,7 @@ function setup() {
     enemy3 = new Enemy(2370, 415);
     enemy4 = new Enemy(3710, 80);
     
-    gamestate=2;
+    gamestate=4;
     points = 0;
     p = new Player(60,402);
     setInterval(timer, 1000);
@@ -142,7 +142,7 @@ function setup() {
 }
 
 function draw() { 
-console.log(p.fake_x, p.y);
+//console.log(p.fake_x, p.y);
 // switch statement with game state - each corresponds to a different "screen"
   switch(gamestate){
       case 0:
@@ -197,7 +197,7 @@ function keyPressed(){
     
 }
 function mousePressed(){
-    //console.log(mouseX,mouseY);
+    console.log(mouseX,mouseY);
 }
 
 //debug screen
@@ -231,6 +231,7 @@ function startScreen(){
 }
 
 function hubScreen(){
+    textSize(20);
     hitmap = hub_hm;
     image(hitmap,0,0);
     image(hub_bg,0,0);
@@ -540,17 +541,14 @@ class Player{
                 front_leaves_x = -1156;
                 this.x += 4;
                 scrolling = false;
-                //console.log(sand_x,fg_x);
                 sand_x = -1156;
                 fg_x = -2312;
             }
         }
-        if (keyIsDown(32) && this.isPixelSolid(this.middleX, this.down)) {
-          this.ySpeed = -10;
+        if (keyIsDown(32) && this.isPixelSolid(this.middleX, this.down)){
+            this.ySpeed = -10;
         }
-        // if (this.death_detection(this.fake_x, this.middleY)) {
-        //     gamestate = ;
-        // }
+        
 
     }
     handleFallJumpMovement() {
@@ -607,57 +605,49 @@ class Player{
     handleDoorMovement(){
         if(gamestate == 1){
         //check if a door was entered
-        if(this.isDoor(this.middleX, this.up) && (keyIsDown(38) || keyIsDown(87)) && !this.locked && this.middleX > 52 && this.middleX < 94){
+        if(this.isDoor(this.middleX, this.up) && (keyIsDown(38) || keyIsDown(87)) && !this.locked && this.middleX < 140 && this.middleX > 60){
             console.log('entered selection door');
-            //gamestate = 0;
+            gamestate = 0;
         }
-        if(mouseX > 52 && mouseX < 94 && mouseY > 376 && mouseY < 443){
+        if(mouseX > 60 && mouseX < 114 && mouseY > 359 && mouseY < 443){
             fill(0,150);
-            rect(35,350,80,20);
+            rect(48,330,80,20);
             fill(255);
-            text('main menu',45,365);
+            text('main menu',58,345);
         }
-        if(this.isDoor(this.middleX, this.up) && (keyIsDown(38) || keyIsDown(87)) && !this.locked && this.middleX > 401 && this.middleX < 442){
+        if(this.isDoor(this.middleX, this.up) && (keyIsDown(38) || keyIsDown(87)) && !this.locked && this.middleX < 455 && this.middleX > 370){
             console.log('entered lvl1 door');
             gamestate = 2;
             p.x = 75;
             p.y = 400;
         }
-        if(mouseX > 401 && mouseX < 442 && mouseY > 232 && mouseY < 297){
+        if(mouseX > 401 && mouseX < 455 && mouseY > 208 && mouseY < 295){
             fill(0,150);
-            rect(390,200,60,20);
+            rect(400,180,60,20);
             fill(255);
-            text('level 1',405,215);
+            text('level 1',415,195);
         }
-        if(this.isDoor(this.middleX, this.up) && (keyIsDown(38) || keyIsDown(87)) && !this.locked && this.middleX > 522 && this.middleX < 563){
+        if(this.isDoor(this.middleX, this.up) && (keyIsDown(38) || keyIsDown(87)) && !this.locked && this.middleX > 717 && this.middleX < 784 && this.middleY < 300){
             console.log('entered lvl2 door');
-            //gamestate = 3;
+            gamestate = 3;
+            p.x = 75;
+            p.y = 400;
         }
-        if(mouseX > 522 && mouseX < 563 && mouseY > 334 && mouseY < 432){
+        if(mouseX > 697 && mouseX < 755 && mouseY > 108 && mouseY < 199){
             fill(0,150);
-            rect(515,310,60,20);
+            rect(698,80,60,20);
             fill(255);
-            text('level 2',527,325);
+            text('level 2',710,95);
         }
-        if(this.isDoor(this.middleX, this.up) && (keyIsDown(38) || keyIsDown(87)) && !this.locked && this.middleX > 670 && this.middleX < 712 && this.middleY > 300){
+        if(this.isDoor(this.middleX, this.up) && (keyIsDown(38) || keyIsDown(87)) && !this.locked && this.middleX > 697 && this.middleX < 785 && this.middleY > 300){
             console.log('entered minigame door');
             gamestate = 4;
         }
-        if(mouseX > 670 && mouseX < 712 && mouseY > 456 && mouseY < 523){
+        if(mouseX > 670 && mouseX < 724 && mouseY > 439 && mouseY < 521){
             fill(0,150);
-            rect(660,430,70,20);
+            rect(660,410,70,20);
             fill(255);
-            text('minigame',670,443);
-        }
-        if(this.isDoor(this.middleX, this.up) && (keyIsDown(38) || keyIsDown(87)) && !this.locked && this.middleX > 692 && this.middleX < 744 && this.middleY < 300){
-            console.log('entered boss door');
-            //gamestate = 5;
-        } 
-        if(mouseX > 692 && mouseX < 744 && mouseY > 120 && mouseY < 203){
-            fill(0,150);
-            rect(690,90,50,20);
-            fill(255);
-            text('boss',701,105);
+            text('minigame',670,423);
         }
         else if(this.isDoor(this.middleX, this.up) && (keyIsDown(38) || keyIsDown(87)) && this.locked){
             noStroke();
@@ -671,7 +661,6 @@ class Player{
     isPixelSolid(x,y){
             let temp = red(hitmap.get(x,y));
             if (temp == 0) {
-            //console.log(temp);
             return true;
             }
             return false;
@@ -756,7 +745,7 @@ class Sprite {
         if(frameCount%10==0){
             
             this.index = this.index + this.speed;
-            console.log(action);
+            //console.log(action);
             if(gamestate == 0){
                 if(this.animation == aniR){
                     if(this.index > 6 || this.index < 3){
